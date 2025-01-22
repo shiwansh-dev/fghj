0afd1dd1fb5500cf89cae46d47869f1b68f64429
shiwansh-dev <development.agromach@gmail.com>
2025-01-22 21:45:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,43 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(5000, () => {
-    console.log("Server is running on http://localhost:5000");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve API on the homepage
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the WebSocket server API!" });
 });
+
+// WebSocket logic
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("offer", (offer) => {
+        socket.broadcast.emit("offer", offer);
+    });
+
+    socket.on("answer", (answer) => {
+        socket.broadcast.emit("answer", answer);
+    });
+
+    socket.on("ice-candidate", (candidate) => {
+        socket.broadcast.emit("ice-candidate", candidate);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+// Listen on the dynamic Render port
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
